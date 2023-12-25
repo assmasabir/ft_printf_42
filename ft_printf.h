d4975ea25670299f39c29e136be65e699b8520e9
asabir <asabir@e2r4p5.1337.ma>
2023-12-25 17:21:30 +0100

    h

--- ft_printf.h
+++ ft_printf.h
@@ -6,20 +6,26 @@
 /*   By: asabir <asabir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 01:28:21 by asabir            #+#    #+#             */
-/*   Updated: 2023/12/24 22:40:17 by asabir           ###   ########.fr       */
+/*   Updated: 2023/12/25 17:09:22 by asabir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
 
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+int	ft_printf(const char *format, ...);
 int				ft_putstr(char *str);
 int				ft_putchar(char c);
 int				ft_putnbr(int nb);
-int				ft_putnbr_unsigned(int nb);
+int				ft_putnbr_unsigned(unsigned int nb);
 unsigned int	ft_size(int nb);
 int				ft_printpercent(void);
 int				ft_hexamn(unsigned int nb);
 int				ft_hexamj(unsigned int nb);
+
+#endif
--- ft_printf_outils2.c
+++ ft_printf_outils2.c
@@ -6,27 +6,40 @@
 /*   By: asabir <asabir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 14:25:28 by asabir            #+#    #+#             */
-/*   Updated: 2023/12/25 14:25:29 by asabir           ###   ########.fr       */
+/*   Updated: 2023/12/25 17:21:01 by asabir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr_unsigned(int nb)
+int	ft_putnbr_unsigned(unsigned int nb)
 {
 	int	len;
 
-	len = ft_size(nb);
+	len = ft_size_unsigned(nb);
 	if (nb > 9)
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		ft_putnbr_unsigned(nb / 10);
+		ft_putnbr_unsigned(nb % 10);
 	}
 	else
 		ft_putchar(nb + 48);
 	return (len);
 }
+unsigned int	ft_size_unsigned(unsigned int nb)
+{
+	unsigned int	len;
 
+	len = 0;
+	if (nb == 0)
+		return (1);
+	while (nb != 0)
+	{
+		nb /= 10;
+		len++;
+	}
+	return (len);
+}
 unsigned int	ft_size(int nb)
 {
 	unsigned int	len;
